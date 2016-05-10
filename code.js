0f6925e7edaa58613766018d71cc37695adfbb35
LKWLaLa <lkwlala@gmail.com>
2016-05-10 13:27:34 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function favIceCream() {
+  return "I love mint chocolate chip";
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+
+function roundDown(num) {
+  return Math.floor(num);
+}
+
+function theTruth() {
+  return true;
+}
